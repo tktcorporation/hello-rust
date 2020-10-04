4a1e2000d2e71527ec9ab894555c95e1f8063dc5
tkt <tktcorporation.go@gmail.com>
2020-10-04 13:35:48 +0900

    print input txt

---
+++ guessing_game/target/debug/deps/guessing_game-6484f09d3a679650.d
@@ -0,0 +1,5 @@
+/app/guessing_game/target/debug/deps/guessing_game-6484f09d3a679650: src/main.rs
+
+/app/guessing_game/target/debug/deps/guessing_game-6484f09d3a679650.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/app/guessing_game/target/debug/guessing_game: /app/guessing_game/src/main.rs
