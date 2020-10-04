6657a979f1ab70b71afd5bb3aa69168a2cde2587
tkt <tktcorporation.go@gmail.com>
2020-10-04 14:17:52 +0900

    Guess the number!

---
+++ guessing_game/target/rls/debug/deps/guessing_game-3de30ab356236eed.d
@@ -0,0 +1,5 @@
+/Users/tkt/workspace/tkt/hello-rust/guessing_game/target/rls/debug/deps/guessing_game-3de30ab356236eed.rmeta: src/main.rs
+
+/Users/tkt/workspace/tkt/hello-rust/guessing_game/target/rls/debug/deps/guessing_game-3de30ab356236eed.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/rls/debug/deps/guessing_game-47688c94e8e88be1.d
@@ -0,0 +1,5 @@
+/Users/tkt/workspace/tkt/hello-rust/guessing_game/target/rls/debug/deps/guessing_game-47688c94e8e88be1.rmeta: src/main.rs
+
+/Users/tkt/workspace/tkt/hello-rust/guessing_game/target/rls/debug/deps/guessing_game-47688c94e8e88be1.d: src/main.rs
+
+src/main.rs:
