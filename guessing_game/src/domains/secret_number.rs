db6282af39b42583ecc0c40d234adce897431879
tkt <tktcorporation.go@gmail.com>
2020-10-11 18:50:35 +0000

    impl some methods to secret number

--- guessing_game/src/domains/secret_number.rs
+++ guessing_game/src/domains/secret_number.rs
@@ -1,7 +1,32 @@
-struct SecretNumber {
+use std::cmp::Ordering;
+
+pub struct SecretNumber {
     value: i32,
 }
 
 pub fn build(value: i32) -> SecretNumber {
     SecretNumber { value }
+}
+
+impl SecretNumber {
+    pub fn get_number(&self) -> i32 {
+        self.value
+    }
+
+    pub fn check(&self, guess: i32) -> bool {
+        match guess.cmp(&self.value) {
+            Ordering::Less => {
+                println!("Too small!"); //小さすぎ！
+                false
+            }
+            Ordering::Greater => {
+                println!("Too big!"); //大きすぎ！
+                false
+            }
+            Ordering::Equal => {
+                println!("You win!"); //やったね！
+                true
+            }
+        }
+    }
 }
